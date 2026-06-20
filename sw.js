fb00799d09c52ea2b95a0c6f26bb14f7ca5b23ea
wenhaoh45-coder <wenhaoh45@gmail.com>
2026-06-20 12:36:08 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,46 @@
-const CACHE_NAME = "family-bistro-v3";
+const CACHE_NAME = "family-bistro-v4";
+const DISH_IMAGES = [
+  "stir-fried-beef",
+  "pepper-pork",
+  "red-braised-pork",
+  "stir-fried-ribs",
+  "tea-oil-chicken",
+  "sizzling-chicken",
+  "sizzling-ribs",
+  "chopped-chili-fish-head",
+  "dry-pot-beef-radish",
+  "pork-ball-pea-shoot-soup",
+  "chili-cured-pork",
+  "stir-fried-sausage",
+  "tomato-beef-brisket",
+  "sweet-sour-ribs",
+  "green-pepper-pork",
+  "pickled-cabbage-beef-mince",
+  "double-pepper-beef-mince",
+  "ants-climbing-tree",
+  "poached-shrimp",
+  "cold-pig-ear",
+  "steamed-egg",
+  "hand-torn-cabbage",
+  "sizzling-cauliflower",
+  "seasonal-greens",
+  "stir-fried-eggplant",
+  "stir-fried-yardlong-beans",
+  "mapo-tofu",
+  "pork-bone-soup",
+  "coconut-chicken",
+  "sour-soup-beef-hotpot",
+  "mashed-pepper-century-egg",
+  "tiger-skin-pepper",
+  "tofu-ball-soup",
+  "tomato-egg-soup",
+  "chive-yellow-eggs",
+  "dumplings",
+  "luosifen",
+  "instant-noodles",
+  "changde-beef-noodles",
+].map((id) => `./assets/dishes/${id}.webp`);
+
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -7,6 +49,7 @@ const APP_SHELL = [
   "./cloud-config.js",
   "./manifest.webmanifest",
   "./assets/app-icon.svg",
+  ...DISH_IMAGES,
 ];
 
 self.addEventListener("install", (event) => {
